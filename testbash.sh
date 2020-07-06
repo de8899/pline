e07d1b1db8957a9aa2716817771aa78cc26dc49d
derwin8899 <derwin8899@gmail.com>
2020-07-05 18:27:45 -0700

    more lines

--- testbash.sh
+++ testbash.sh
@@ -2,6 +2,11 @@
 echo "bash script start..."
 sudo uname -a > /tmp/dje.txt
 sudo echo "dje" >> /dje.txt
+sudo touch /tmp/file2
 touch /tmp/file1
-sudo echo "Hi!" >> /tmp/file1
-echo "bash script end!!"
+sudo echo "Hi!" >> /tmp/file2
+echo "Hi no sudo" >> /tmp/file1
+echo "bash script ended!!"
+echo "here's the date: " 
+date
+echo "done"
