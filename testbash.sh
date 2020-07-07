eea659ded40fd86f05d2248745c8efd69e90e201
derwin8899 <derwin8899@gmail.com>
2020-07-06 20:54:47 -0700

    vm extension try1

--- testbash.sh
+++ testbash.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-echo "bash script start..."
-mkdir /var/dje/
-sudo uname -a > /var/dje/dje.txt
-sudo echo "dje" >> /var/dje/dje.txt
-sudo touch /var/file2
-touch /var/file1
-sudo echo "Hi!" >> /var/file2
-echo "Hi no sudo" >> /var/file1
-echo "bash script ended!!"
-echo "here's the date: " 
-date
-echo "done with this one!"
+echo "starting testbash!"
+touch /tmp/dje.txt
+echo "Hello dje" >> /tmp/dje.txt
+sudo touch /tmp/file1
+sudo echo "hello from sudo" >> /tmp/file1
+su touch /tmp/file2
+su echo "hello from su" >> /tmp/file2
+uname -a >> /tmp/dje.txt
+uname -a >> /tmp/file1
+uname 1 >> /tmp/file2
+echo "done"
